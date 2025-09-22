83286978b2a36102d57af5d9c9060cb857292b8e
elenarivero <145675391+elenarivero@users.noreply.github.com>
2025-09-22 09:44:30 +0200

    Tema 1

---
+++ PROG-Unit01/src/unit01/Exercise01.java
@@ -0,0 +1,28 @@
+package unit01;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		// Creation of the Scanner
+		Scanner reader = new Scanner(System.in);
+
+		// Variable to store a number
+		int number;
+
+		// Ask the user a number
+		System.out.println("Introduce a number:");
+
+		// Reading number
+		number = reader.nextInt();
+
+		// Print the final message
+		System.out.println("Number introduced by user: " + number);
+		
+		// Closing Scanner
+		reader.close();
+
+	}
+
+}
