698240730e2ab9e996f2ebfb3c0db18f9c7ed5b0
elenarivero <145675391+elenarivero@users.noreply.github.com>
2025-09-26 12:29:25 +0200

    Ejercicio 15 corregido

---
+++ PROG-Unit01/src/unit01/Exercise15.java
@@ -0,0 +1,36 @@
+package unit01;
+
+import java.util.Scanner;
+
+public class Exercise15 {
+
+	public static void main(String[] args) {
+		/*
+		 * Pedir precio al usuario (tiene decimales) Mostrar el precio con el IVA
+		 */
+		// Creamos el Scanner
+		Scanner reader = new Scanner(System.in);
+
+		// Constante para almacenar el valor del IVA
+		final int IVA = 21;
+
+		// Precio a leer de consola
+		double precio;
+
+		// Variable donde almacenar el precio total con el IVA incluido
+		double precioConIVA;
+
+		// Le pedimos el precio al usuario
+		System.out.println("Introduzca el precio:");
+		precio = reader.nextDouble();
+
+		// Calculamos el precion con IVA incluido
+		precioConIVA = precio + precio * IVA / 100;
+		System.out.println("El precio con IVA es: " + precioConIVA);
+
+		// Cerramos Scanner
+		reader.close();
+
+	}
+
+}
